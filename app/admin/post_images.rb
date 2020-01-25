df5a296f245154dc216a2f1aff7e8fdd2fc40453
NLCaceres <NLCaceres21@Gmail.com>
2020-01-25 09:12:40 -0800

    Basic Index and Show config

--- app/admin/post_images.rb
+++ app/admin/post_images.rb
@@ -1,4 +1,24 @@
 ActiveAdmin.register PostImage do
+  index do 
+    column :post
+    column :image_url
+    column :alt_text
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :post do |post_image|
+        post = Post.find(post_image.post_id)
+        link_to(post.title, admin_post_path(post))
+      end
+      row :alt_text
+      row :image_url do |image|
+        image_tag image.image_url
+      end
+    end
+  end
+
   permit_params :image_url, :alt_text, :post_id
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
