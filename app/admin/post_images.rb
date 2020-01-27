31358b21a358435999729f8e6f907c46529f5611
NLCaceres <NLCaceres21@Gmail.com>
2020-01-27 15:20:02 -0800

    ActiveAdmin customization with images

--- app/admin/post_images.rb
+++ app/admin/post_images.rb
@@ -1,20 +1,23 @@
 ActiveAdmin.register PostImage do
+  config.per_page = [10, 25, 40, 50]
+
   index do 
+    selectable_column 
     column :post
     column :image_url
     column :alt_text
     actions
   end
 
-  show do
+  show title: :alt_text do
     attributes_table do
       row :post do |post_image|
         post = Post.find(post_image.post_id)
         link_to(post.title, admin_post_path(post))
       end
-      row :alt_text
+      #row :alt_text
       row :image_url do |image|
-        image_tag image.image_url
+        image_tag image.image_url, size: "600x600"
       end
     end
   end
